067aa6f6aa5c5e38d37751b85550173312d7efac
Rahul Nakarani <w0823305@myscc.ca>
2023-11-04 16:45:05 -0400

    Add project files.

---
+++ RahulBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RahulBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
